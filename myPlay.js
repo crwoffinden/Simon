659bcf32eda72c7ad1ac377dc2242c50f306c7ac
crwoffinden <crwoffinden@gmail.com>
2023-03-06 16:31:19 -0700

    modified:   myPlay.js

--- myPlay.js
+++ myPlay.js
@@ -32,3 +32,32 @@ class Button {
         })
     }
 }
+
+class Game {
+    buttons;
+    allowPlayer;
+    sequence;
+    playerPlaybackPos;
+    mistakeSound;
+
+    constructor() {
+        this.buttons = new Map();
+        this.allowPlayer = false;
+        this.sequence = [];
+        this.playerPlaybackPos = 0;
+        this.mistakeSound = loadSound('error.mp3')
+
+        document.querySelectorAll('.game-button').forEach((el, i) => {
+            if (i < btnDescriptions.length) {
+                this.buttons.set(el.id, new Button(btnDescriptions[i], el));
+            }
+        });
+
+        const playerNameEl = document.querySelector('.player-name');
+        playerNameEl.textContent = this.getPlayerName();
+    }
+
+    getPlayerName() {
+        return localStorage.getItem('userName') ?? 'Mystery player';
+    }
+}

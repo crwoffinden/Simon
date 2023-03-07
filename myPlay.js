e8c23fc4eb46adefc53cc5b07b06c918b417e112
crwoffinden <crwoffinden@gmail.com>
2023-03-06 17:42:50 -0700

    modified:   myPlay.js

--- myPlay.js
+++ myPlay.js
@@ -1,8 +1,8 @@
 const btnDescriptions = [
-    {file: 'sound1.mp3', hue: 120 },
-    {file: 'sound2.mp3', hue: 0},
-    {file: 'sound3.mp3', hue: 60},
-    {file: 'sound4.mp3', hue: 240},
+    { file: 'sound1.mp3', hue: 120 },
+    { file: 'sound2.mp3', hue: 0 },
+    { file: 'sound3.mp3', hue: 60 },
+    { file: 'sound4.mp3', hue: 240 },
 ];
 
 class Button {
